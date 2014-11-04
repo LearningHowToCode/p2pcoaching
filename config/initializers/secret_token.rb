96b322cb6379524d5d42d03023df8ae576686e3f
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2014-11-04 15:10:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Belluga2::Application.config.secret_key_base = 'f96d821c8d7a69075bf68fdf46ddca412a8017b4d8ed3cbb715a88e8a8d22c48af1804c0c5a6fa14862dc6ab1fd76b8e5069fe798b58f7ea64c655a095554ed1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Belluga2::Application.config.session_store :cookie_store, key: '_Belluga2_session'
