ca1ffa7e7dabf42b7469628d128e46a8348d73d7
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2014-11-05 17:08:14 -0500

    Victor 1Revise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105212719) do
+ActiveRecord::Schema.define(version: 20141105214451) do
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "country"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tutors", force: true do |t|
     t.string   "name"
