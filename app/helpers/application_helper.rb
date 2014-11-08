5485f916ecbdf4f6859e530b6fa2123e4d27451c
Victor Cortes <ie.cortex@gmail.com>
2014-11-08 11:10:15 -0500

    My Profile goes to show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def link_to_my_account
     if current_user.tutor?
-      link_to 'My Profile', edit_tutor_path(current_user.profile)
+      link_to 'My Profile', tutor_path(current_user.profile)
     elsif current_user.student?
-      link_to 'My Profile', edit_student_path(current_user.profile)
+      link_to 'My Profile', student_path(current_user.profile)
     end
   end
 end
