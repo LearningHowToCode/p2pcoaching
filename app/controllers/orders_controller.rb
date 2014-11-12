da40c6e3bc3ee376e52611652321a46ddce15ce3
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2014-11-12 14:05:45 -0500

    Worked out Stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,6 +42,23 @@ class OrdersController < ApplicationController
     @order.buyer_id = current_user.profile.id
     @order.seller_id = @lesson.tutor.id
 
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        :amount => 50.floor,
+        :currency => "usd",
+        :card => token
+        )
+      flash[:notice] = "Thanks for ordering!"
+    rescue Stripe::CardError => e
+      flash[:danger] = e.message
+    end
+    
+
+
+
     respond_to do |format|
       if @order.save
         format.html { redirect_to root_path, notice: 'Thank you for making a reservation' }
