11d18cdc7a76210abc89663d2cfef7545d7575ec
Victor Cortes <ie.cortex@gmail.com>
2014-11-19 10:50:31 -0500

    Validates student fields

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,9 +4,19 @@ class Student < ActiveRecord::Base
 
   has_many :purchases, class_name: 'Order', foreign_key: 'buyer_id'
 
+  validates_presence_of "name", "country", "preference"
+  validate :tool_presence
+
   def country_name
     return '' if self.country.nil?
     country = ISO3166::Country[self.country]
     country.translations[I18n.locale.to_s] || country.name
+  end
+
+  private
+  def tool_presence
+    if self.skype_id.blank? && self.gmail_address.blank?
+      self.errors.add(:base, 'You have to provide either your Skype ID or your Gmail address (for Google Hangouts).')
+    end
   end
 end
