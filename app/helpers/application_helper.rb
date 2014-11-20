810df84c56473adcf8f546cc76926c12b034ba2c
Victor Cortes <ie.cortex@gmail.com>
2014-11-20 18:18:16 -0500

    Redirect to profile edit from My Profile when no completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
 
-  def link_to_my_account
+  def link_to_my_profile
     if current_user.tutor?
-      link_to 'My Profile', tutor_path(current_user.profile)
+      if current_user.profile.completed_profile?
+        link_to 'My Profile', tutor_path(current_user.profile)
+      else
+        link_to 'My Profile', edit_tutor_path(current_user.profile)
+      end
     elsif current_user.student?
       link_to 'My Profile', student_path(current_user.profile)
     end
