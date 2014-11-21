4c6e357ef7f9d7dfded23666b16d6425ed0a7e76
Victor Cortes <ie.cortex@gmail.com>
2014-11-21 16:13:55 -0500

    Do not show lesson for today

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -10,7 +10,7 @@ class TutorsController < ApplicationController
   end
 
   def show
-    @lessons = @tutor.lessons.where(['day >= ?', Date.today])
+    @lessons = @tutor.lessons.where(['day > ?', Date.today])
     respond_with(@tutor)
   end
 
