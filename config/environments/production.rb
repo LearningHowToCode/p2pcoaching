dbb52112e61f395abaea3ce964e7b31c7eddbc75
Victor Cortes <ie.cortex@gmail.com>
2014-11-25 10:25:10 -0500

    Setup mandrill in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,18 @@ Belluga2::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'www.pairbis.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      enable_starttls_auto: true,
+      address: "smtp.mandrillapp.com",
+      port: 25,
+      authentication: :login,
+      user_name: ENV['SMTP_USER_NAME'],
+      password: ENV['SMTP_API_KEY'],
+      domain: 'www.pairbis.com'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
