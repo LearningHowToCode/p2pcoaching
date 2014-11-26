9624d37c1ab0a526d2a2d89a8e87432ffc306148
Victor Cortes <ie.cortex@gmail.com>
2014-11-26 12:32:09 -0500

    Remove routing and account number

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -41,7 +41,7 @@ class TutorsController < ApplicationController
                                   :undergraduate_institution, :undergraduate_major,
                                   :graduate_institution, :graduate_study_field,
                                   :subject, :long_bio, :skype_id, :gmail_address,
-                                  :languages, :routing_number, :account_number)
+                                  :languages)
   end
 
   def normalized_tutor_params
