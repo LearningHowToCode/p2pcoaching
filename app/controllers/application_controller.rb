15b74f08b5d6c917074b5aa424d7e588a0ab4593
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2014-12-03 15:54:03 -0500

    Added language options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:profile_type, :name]
   end
+  
+  def set_locale
+  	I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+  	{ locale: I18n.locale }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Belluga2::Application.routes.draw do
 
-  resources :lessons, except: [:index, :show] do
-    resources :orders
+  scope "(:locale)", locale: /en|ja/ do
+
+    resources :lessons, except: [:index, :show] do
+      resources :orders
+    end
+    resources :students, except: [:new, :create, :index]
+    resources :tutors, except: [:new, :create]
+
+    devise_for :users, :controllers => { :registrations => "registrations" }
+    devise_scope :user do
+      get ':profile/sign_up', to: 'devise/registrations#new', as: 'new_registration',
+          constraints: { profile: /tutor|student/ }
+    end
+
+    get 'sales', to: "orders#sales"
+    get 'purchases', to: "orders#purchases"
+    get 'how_it_works', to: 'pages#how_it_works'
+    get 'faqs', to: 'pages#faqs'
+
+    root "pages#home"
   end
-  resources :students, except: [:new, :create, :index]
-  resources :tutors, except: [:new, :create]
-
-  devise_for :users, :controllers => { :registrations => "registrations" }
-  devise_scope :user do
-    get ':profile/sign_up', to: 'devise/registrations#new', as: 'new_registration',
-        constraints: { profile: /tutor|student/ }
-  end
-
-  get 'sales', to: "orders#sales"
-  get 'purchases', to: "orders#purchases"
-  get 'how_it_works', to: 'pages#how_it_works'
-  get 'faqs', to: 'pages#faqs'
-
-  root "pages#home"
 end
