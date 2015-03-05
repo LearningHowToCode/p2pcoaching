f517291681ab5bf224a48c24d10e7f1a7a4338b0
Juan David Pastás Rivera <juandavid1707@gmail.com>
2015-03-05 16:59:14 -0500

    Add timezone labels and calculations.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,9 +24,46 @@ module ApplicationHelper
     time.strftime('%H:%M')
   end
 
+  def format_lesson_day(lesson, viewer = current_user)
+    viewer_lesson_time(lesson, viewer).to_date
+  end
+
+  def format_lesson_time(lesson, viewer = current_user)
+    viewer_lesson_time(lesson, viewer).strftime('%H:%M')
+  end
+
+  def viewer_lesson_time(lesson, viewer = current_user)
+    date           = lesson.day
+    time_part      = lesson.start_time
+
+    time = date.strftime('%Y-%m-%d ') + time_part.strftime('%H:%M')
+    time = Time.zone.parse time
+
+    tutor_offset   = ActiveSupport::TimeZone[lesson.tutor.timezone].try :utc_offset
+
+    viewer_zone_s  = viewer ? viewer.timezone : default_zone_abbr
+    viewer_offset  = ActiveSupport::TimeZone[viewer_zone_s].try :utc_offset
+
+    time + viewer_offset - tutor_offset
+  end
 
   def timezones
     ActiveSupport::TimeZone.us_zones.reverse +
       [ActiveSupport::TimeZone['Tokyo'], ActiveSupport::TimeZone['Beijing']]
   end
+
+  def user_zone_s
+    (user_zone.to_s.presence || default_zone_s)
+      .gsub(/\(|\)/, '')
+  end
+
+  def user_zone
+    if current_user
+      ActiveSupport::TimeZone[current_user.timezone]
+    end
+  end
+
+  def default_zone_abbr; 'Eastern Time (US & Canada)' end
+  def default_zone; ActiveSupport::TimeZone[default_zone_abbr] end
+  def default_zone_s; default_zone.to_s end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   after_create :create_profile
+  delegate :timezone, to: :profile
 
   def student?
     self.profile_type == 'Student'
