a24ceb64ed57a6f36ed6310795d765a34e60e8e5
Juan David Pastás Rivera <juandavid1707@gmail.com>
2015-03-17 19:03:45 -0500

    Rating between 1 and 5.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,16 @@
 class Review < ActiveRecord::Base
   belongs_to :lesson
   
+  validate do |r|
+    errors.add(:rating, I18n.t(:rating_should_be_between_1_and_5)) if r.rating < 1 || r.rating > 5
+  end
+
   after_save :update_tutor_rating
 
   def update_tutor_rating
-    reviews = lesson.tutor.lessons.reviews
-    lesson.tutor.update_column :rating, reviews.sum(&:rating) / reviews.length
+    if lesson && lesson.tutor
+      reviews = lesson.tutor.lessons.reviews
+      lesson.tutor.update_column :rating, reviews.sum(&:rating) / reviews.length
+    end
   end
 end
