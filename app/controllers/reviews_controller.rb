85206968a3a0c499b701e12e0c1025e55e2e6412
Juan David Pastás Rivera <juandavid1707@gmail.com>
2015-03-18 15:17:06 -0500

    Authorize review.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   respond_to :html
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+
   rescue_from('ActiveRecord::RecordNotFound'){ |e| redirect_to root_path, notice: e.message }
 
   def index
@@ -15,26 +16,46 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
     @lesson = Lesson.find(params[:lesson_id])
+
+    # TODO use authorization solution
+    return redirect_to(root_path, notice: t(:unauthorized) ) if unauthorized_user
+
     respond_with(@review)
   end
 
   def edit
+    # TODO use authorization solution
     @lesson = @review.lesson
+    return redirect_to(root_path, notice: t(:unauthorized) ) if unauthorized_user
   end
 
   def create
     @review = Review.new(review_params)
     @review.lesson_id = params[:lesson_id]
+
+    # TODO use authorization solution
+    @lesson = @review.lesson
+    return redirect_to(root_path, notice: t(:unauthorized) ) if unauthorized_user
+
     @review.save
     redirect_to root_path
   end
 
   def update
+
+    # TODO use authorization solution
+    @lesson = @review.lesson
+    return redirect_to(root_path, notice: t(:unauthorized) ) if unauthorized_user
+
     @review.update(review_params)
-    respond_with(@review)
+    redirect_to root_path
   end
 
   def destroy
+    # TODO use authorization solution
+    @lesson = @review.lesson
+    return redirect_to(root_path, notice: t(:unauthorized) ) if unauthorized_user
+
     @review.destroy
     redirect_to root_path
   end
@@ -46,6 +67,11 @@ class ReviewsController < ApplicationController
 
     def review_params
       params.require(:review).permit(:review, :rating)
+    end
+
+    # TODO use authorization solution
+    def unauthorized_user
+      !current_user || @lesson.student != current_user.profile
     end
     
 end
