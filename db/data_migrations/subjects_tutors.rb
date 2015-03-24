0e3eb262858581d71dd09457b825ed9bf79caa9d
Victor Cortes <ie.cortex@gmail.com>
2015-03-23 20:09:20 -0500

    Add task to create other subjects

--- db/data_migrations/subjects_tutors.rb
+++ db/data_migrations/subjects_tutors.rb
@@ -1,6 +1,20 @@
+puts 'Create own subjects'
+Tutor::SUBJECTS.each do |subject|
+  Subject.create name: subject, own: true
+  print '.'
+end
+
+puts 'Create current subjects'
+Tutor.pluck(:subject).join(', ').split(',').map(&:strip).uniq.each do |subject|
+  Subject.create name: subject unless Tutor::SUBJECTS.include?(subject)
+  print '.'
+end
+puts 'created!'
+
+puts 'Set tutors subjects set in join table'
 Tutor.all.each do |t|
   t.subjects = Subject.where{ name.in t.subject.try(:split, ', ') }
   t.save
   print '.'
 end
-puts 'Tutors subjects set in join table!'
+puts 'done!'
