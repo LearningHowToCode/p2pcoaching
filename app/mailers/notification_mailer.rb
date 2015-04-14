e8a5256d698279559a9a27d0efcec1a25a717b18
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2015-04-13 21:07:16 -0400

    pairbis to p2pcoaching gmail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,12 @@
 class NotificationMailer < ActionMailer::Base
-  default from: 'pairbis@gmail.com', bcc: 'pairbis@gmail.com'
+  default from: 'p2pcoaching2015@gmail.com', bcc: 'p2pcoaching2015@gmail.com'
 
   def lesson_confirmation_to_student(order)
     @order = order
     @tutor = @order.seller
     @student = @order.buyer
 
-    mail(to: @student.user.email, subject: 'Your Pairbis lesson (confirmed)')
+    mail(to: @student.user.email, subject: 'Your P2P Coaching lesson (confirmed)')
   end
 
   def lesson_confirmation_to_tutor(order)
@@ -14,28 +14,28 @@ class NotificationMailer < ActionMailer::Base
     @tutor = @order.seller
     @student = @order.buyer
 
-    mail(to: @tutor.email, subject: 'Your Pairbis lesson (confirmed)')
+    mail(to: @tutor.email, subject: 'Your P2P Coaching lesson (confirmed)')
   end
 
   def welcome_tutor(user)
-    mail(to: user.email, subject: 'Thank you for signing up for Pairbis!')
+    mail(to: user.email, subject: 'Thank you for signing up for P2P Coaching!')
   end
 
   def welcome_student(user)
-    mail(to: user.email, subject: 'Thank you for signing up for Pairbis!')
+    mail(to: user.email, subject: 'Thank you for signing up for P2P Coahing!')
   end
 
   def contact_tutor(tutor, info)
     @tutor = tutor
     @info = info
 
-    mail(to: 'pairbis@gmail.com', subject: 'A student has messaged a tutor!')
+    mail(to: 'p2pcoaching2015@gmail.com', subject: 'A student has messaged a coach!')
   end
 
   def contact_us(info)
     @info = info
 
-    mail(to: 'pairbis@gmail.com', subject: 'A student has signed up on the ESL page!')
+    mail(to: 'p2pcoaching2015@gmail.com', subject: 'A student has signed up on the ESL page!')
   end
 
   def remind_review(lesson)
@@ -43,6 +43,6 @@ class NotificationMailer < ActionMailer::Base
     @student = lesson.student
     @tutor   = lesson.tutor
 
-    mail(to: @student.email, subject: 'Please review your tutor!')
+    mail(to: @student.email, subject: 'Please review your coach!')
   end
 end
